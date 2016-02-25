2624013f89b04d195e5dfd1b6badb7a7770df87e
albertoceb <aceballosr@gmail.com>
2016-02-25 18:06:09 -0300

    requerimiento 3

--- src/main/java/games/tdd/testing/cat/TicTacToe.java
+++ src/main/java/games/tdd/testing/cat/TicTacToe.java
@@ -4,12 +4,18 @@ public class TicTacToe {
 
 	private Character[][] board = {{'\0', '\0', '\0'},{'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
 	private char lastPlayer = '\0';
+	private static final int SIZE = 3;
 
-	public void play(int x, int y) {
+
+	public String play(int x, int y) {
 		checkAxis(x);
 		checkAxis(y);
-		setBox(x, y);
 		lastPlayer = nextPlayer();
+		setBox(x, y, lastPlayer);
+		if (isWin()) {
+			return lastPlayer + " is the winner";
+		}
+		return "No winner";
 	}
 
 	private void checkAxis(int axis) {
@@ -18,13 +24,6 @@ public class TicTacToe {
 		}
 	}
 
-	private void setBox(int x, int y) {
-		if (board[x - 1][y - 1] != '\0') {
-			throw new RuntimeException("Box is occupied");
-		} else {
-			board[x - 1][y - 1] = 'X';
-		}
-	}
 
 	public char nextPlayer() {
 		if (lastPlayer == 'X') {
@@ -33,4 +32,30 @@ public class TicTacToe {
 			return 'X';
 	}
 
+	private void setBox(int x, int y, char lastPlayer) {
+		if (board[x - 1][y - 1] != '\0') {
+			throw new RuntimeException("Box is occupied");
+		} else {
+			board[x - 1][y - 1] = lastPlayer;
+		}
+	}
+
+	private boolean isWin() {
+		int playerTotal = lastPlayer * 3;
+		char diagonal1 = '\0';
+		char diagonal2 = '\0';
+		for (int i = 0; i < SIZE; i++) {
+			diagonal1 += board[i][i];
+			diagonal2 += board[i][SIZE - i - 1];
+			if ((board[0][i] + board[1][i] + board[2][i]) == playerTotal) {
+				return true;
+			} else if (playerTotal == (board[i][0] + board[i][1] + board[i][2])) {
+				return true;
+			}
+		}
+		if (diagonal1 == playerTotal || diagonal2 == playerTotal) {
+			return true;
+		}
+		return false;
+	}
 }
