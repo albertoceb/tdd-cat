9eb64c2bc29a6f0945551399560d3d004407c950
albertoceb <aceballosr@gmail.com>
2016-02-25 18:17:57 -0300

    requerimiento 4

--- src/main/java/games/tdd/testing/cat/TicTacToe.java
+++ src/main/java/games/tdd/testing/cat/TicTacToe.java
@@ -6,16 +6,29 @@ public class TicTacToe {
 	private char lastPlayer = '\0';
 	private static final int SIZE = 3;
 
-
 	public String play(int x, int y) {
 		checkAxis(x);
 		checkAxis(y);
 		lastPlayer = nextPlayer();
 		setBox(x, y, lastPlayer);
-		if (isWin()) {
+		if (isWin(x, y)) {
 			return lastPlayer + " is the winner";
+		} else if (isDraw()) {
+			return "The result is draw";
+		} else {
+			return "No winner";
 		}
-		return "No winner";
+	}
+
+	private boolean isDraw() {
+		for (int x = 0; x < SIZE; x++) {
+			for (int y = 0; y < SIZE; y++) {
+				if (board[x][y] == '\0') {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	private void checkAxis(int axis) {
@@ -23,7 +36,6 @@ public class TicTacToe {
 			throw new RuntimeException("X is outside board");
 		}
 	}
-
 
 	public char nextPlayer() {
 		if (lastPlayer == 'X') {
@@ -40,20 +52,17 @@ public class TicTacToe {
 		}
 	}
 
-	private boolean isWin() {
+	private boolean isWin(int x, int y) {
 		int playerTotal = lastPlayer * 3;
-		char diagonal1 = '\0';
-		char diagonal2 = '\0';
+		char horizontal, vertical, diagonal1, diagonal2;
+		horizontal = vertical = diagonal1 = diagonal2 = '\0';
 		for (int i = 0; i < SIZE; i++) {
+			horizontal += board[i][y - 1];
+			vertical += board[x - 1][i];
 			diagonal1 += board[i][i];
 			diagonal2 += board[i][SIZE - i - 1];
-			if ((board[0][i] + board[1][i] + board[2][i]) == playerTotal) {
-				return true;
-			} else if (playerTotal == (board[i][0] + board[i][1] + board[i][2])) {
-				return true;
-			}
 		}
-		if (diagonal1 == playerTotal || diagonal2 == playerTotal) {
+		if (horizontal == playerTotal || vertical == playerTotal || diagonal1 == playerTotal || diagonal2 == playerTotal) {
 			return true;
 		}
 		return false;
